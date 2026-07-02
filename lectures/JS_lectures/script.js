67f665638ba99ba8a9bb6b6d65c48a95c3495ca6
ALPHONS JINCE <alphonsjincea@gmail.com>
2026-07-02 19:41:07 +0530

    Initial commit: Added my HTML and CSS assignment files
    Web Dev practice

---
+++ lectures/JS_lectures/script.js
@@ -0,0 +1,10 @@
+let fullName = "alphons"
+
+if (fullName[0] === "a" && fullName.length > 3) {
+    console.log("It is a good string")
+}
+else {
+    console.log("It is a bad string")
+}
+
+lsjdfklsdfl
