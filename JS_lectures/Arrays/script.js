44cdf96e65a497a0de9f5b52111640c978609012
ALPHONS JINCE <alphonsjincea@gmail.com>
2026-07-08 21:56:22 +0530

    feat: solved find the number of digits in a number

--- JS_lectures/Arrays/script.js
+++ JS_lectures/Arrays/script.js
@@ -1,9 +1,4 @@
-let userNum = prompt("Enter a number");
+let userNum = 287152;
 
-let sum = 0;
-
-for (let num of userNum) {
-    sum = sum + Number(num);
-}
-
-console.log(sum);
+let str = String(userNum);
+console.log(str.length);
