597fe162ba324c5ef8d55853b058f3d9d389ee63
ALPHONS JINCE <alphonsjincea@gmail.com>
2026-07-09 22:25:22 +0530

    feat:solve js only todo

--- JS_lectures/Arrays/script.js
+++ JS_lectures/Arrays/script.js
@@ -1,4 +1,31 @@
-let userNum = 287152;
+let todo = [];
 
-let str = String(userNum);
-console.log(str.length);
+let req = prompt("Enter your request");
+
+while (true) {
+    if (req == "quit") {
+        console.log("You quitted");
+        break;
+    }
+
+    if (req == "list") {
+      console.log("------------------");
+      for (let i = 0; i < todo.length; i++) {
+        console.log(i, todo[i]);
+      }
+      console.log("------------------");
+    } else if (req == "add") {
+      let task = prompt("What to add");
+      todo.push(task);
+      console.log("Task Added");
+    } else if (req == "delete") {
+        let idx = prompt("Enter the index which u want to delete");
+        todo.splice(idx, 1);
+        console.log("Task deleted");
+    } else {
+        console.log("You have entered wrong request");
+    }
+
+    req = prompt("Enter your request");
+    
+}
