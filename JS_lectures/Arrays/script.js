60c348dd2bfec8b3b21fc79e9409923420a215aa
ALPHONS JINCE <alphonsjincea@gmail.com>
2026-07-11 23:52:47 +0530

    feat: functions practice

--- JS_lectures/Arrays/script.js
+++ JS_lectures/Arrays/script.js
@@ -1,22 +1,6 @@
-let maxGuess = Number(prompt("Enter max no. to which u want to guess "));
-
-let maxNumber = Math.floor(Math.random() * maxGuess + 1);
-
-console.log(maxNumber);
-
-let userGuess = Number(prompt("Now guess your number"));
-
-
-while (true) {
-    if (userGuess < maxNumber) {
-        userGuess = Number(prompt("U guessed smaller number guess a higher no."));
-    } else if (userGuess > maxNumber) {
-        userGuess = Number(prompt("U guessed higher number guess a smaller no."));
-    } else if (userGuess === maxNumber) {
-        console.log("U have guessed the right no.");
-        break;
-    } else {
-        userGuess = Number(prompt("Guess wrong request!!"));
-    }
-        
+function printInfo(name,age){
+    console.log(name,age)
 }
+
+printInfo("alphons", 34);
+
