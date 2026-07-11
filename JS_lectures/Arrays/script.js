1e57cfcbeeebeae888bc5c66a529f07f473860cc
ALPHONS JINCE <alphonsjincea@gmail.com>
2026-07-11 18:02:20 +0530

    feat: solve guess random user no.

--- JS_lectures/Arrays/script.js
+++ JS_lectures/Arrays/script.js
@@ -1,5 +1,17 @@
-let num = Math.floor(Math.random() * 5) + 1;
+let maxGuess = Number(prompt("Enter max no. to which u want to guess "));
+
+let maxNumber = Math.floor(Math.random() * maxGuess + 1);
+
+console.log(maxNumber);
+
+let userGuess = Number(prompt("Now guess your number"));
 
 
-
-console.log(num);
+while (true) {
+    if (userGuess == maxNumber) {
+        console.log("You have guessed the right no.", userGuess);
+        break;
+    } else {
+        userGuess = prompt("Guess Again");
+    }
+}
