c7fb5ceb4549c9fac8b6ada32ec41a1a9c5baaf7
ALPHONS JINCE <alphonsjincea@gmail.com>
2026-07-12 23:50:35 +0530

    feat: solve multiple array string space concat functions

--- JS_lectures/Arrays/script.js
+++ JS_lectures/Arrays/script.js
@@ -2,7 +2,15 @@ let arr1 = ["I", "am", "alphons"];
 let arr2 = ["Jince", "from", "bhopal"];
 
 function concat(arr1, arr2) {
-    return arr1.concat(arr2);
+    let final = arr1.concat(arr2);
+
+    let result = "";
+    for (let i = 0; i < final.length; i++){
+        result += final[i] + " ";
+        
+    }
+    return result;
+    
 }
 
 console.log(concat(arr1, arr2));
