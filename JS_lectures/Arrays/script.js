595e25a1ce8caad53d7c2acae5f8a78f35e71036
ALPHONS JINCE <alphonsjincea@gmail.com>
2026-07-16 23:42:31 +0530

    feat: more this scope types
    flj

--- JS_lectures/Arrays/script.js
+++ JS_lectures/Arrays/script.js
@@ -9,5 +9,15 @@ const student = {
     getMarks: () => {
         console.log(this);
         return this.marks;
+    },
+    getInfo1: function () {
+        setTimeout(() => {
+            console.log(this);
+        }, 2000);
+    },
+    getInfo2: function () {
+        setTimeout(function () {
+            console.log(this);
+        }, 2000);
     }
 };
