57cdcc3feea7ad4f9d9e2cf750bfdf30ab6ebdf9
ALPHONS JINCE <alphonsjincea@gmail.com>
2026-07-16 23:35:19 +0530

    feat: this scope in different functions

--- JS_lectures/Arrays/script.js
+++ JS_lectures/Arrays/script.js
@@ -1,10 +1,13 @@
-let start = Number(prompt("Enter a start number"));
-let end = Number(prompt("Enter a end number"));
-
-function random(first, last) {
-    let diff = last - first;
-    let randNum = Math.floor(Math.random() * diff) + first;
-    return randNum;
-}
-
-console.log(random(start, end));
+const student = {
+    name: "Alphons",
+    marks: 95,
+    prop: this,
+    getName: function () {
+        console.log(this);
+        return this.name;
+    },
+    getMarks: () => {
+        console.log(this);
+        return this.marks;
+    }
+};
