ad62fd8fad3884c67c57ab0e9ad9ab7a46d11464
ALPHONS JINCE <alphonsjincea@gmail.com>
2026-07-18 13:52:53 +0530

    feat: solve output of multiple higher order functions

--- JS_lectures/Arrays/script.js
+++ JS_lectures/Arrays/script.js
@@ -1,16 +1,10 @@
-const greet = (name) => {
-  console.log("Hello " + name);
-  return () => {
-    console.log("Bye " + name);
+function multiply(x) {
+  return function (y) {
+    return x * y;
   };
-};
+}
 
-const run = (func) => {
-  console.log("Start");
-  func();
-  console.log("End");
-};
+const double = multiply(2);
 
-const x = greet("Bob");
-
-run(x);
+console.log(double(5));
+console.log(multiply(3)(4));
