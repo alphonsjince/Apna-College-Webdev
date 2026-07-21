bab6f11dfd96fc85ff2269281f2f247518ee5bab
ALPHONS JINCE <alphonsjincea@gmail.com>
2026-07-21 23:50:30 +0530

    feat: learnt deconstructing objects

--- JS_lectures/Arrays/script.js
+++ JS_lectures/Arrays/script.js
@@ -1,5 +1,10 @@
-let names = ["tony", "bruce", "peter", "steve", "hulk", "thor"];
+const student = {
+  name: "karan",
+  age: 13,
+  class: 8,
+  subjects: ["English", "hindi", "maths"],
+  username: "karan@123",
+  password: "abcd"
+};
 
-let [winner, runup] = names;
-
-console.log(winner);
+let { username: user, password: hidden, city: place = "kerala" } = student;
