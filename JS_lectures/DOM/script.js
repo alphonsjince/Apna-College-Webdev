48c6fc16522c9aa7eaf7ba99658713607150764f
ALPHONS JINCE <alphonsjincea@gmail.com>
2026-07-23 23:40:41 +0530

    feat: DOM multiple css and html using js only

--- JS_lectures/DOM/script.js
+++ JS_lectures/DOM/script.js
@@ -14,4 +14,19 @@ h3.innerText = "im blue h3";
 
 document.querySelector("body").append(h3);
 
-h3.classList.add("h3")
+h3.classList.add("h3");
+
+
+
+let div = document.createElement("div");
+let h1 = document.createElement("h1");
+let p = document.createElement("p");
+
+h1.innerText = "hey im h1";
+p.innerText = "hey im p2";
+
+div.append(h1);
+div.append(p);
+div.classList.add("box");
+
+document.querySelector("body").append(div);
