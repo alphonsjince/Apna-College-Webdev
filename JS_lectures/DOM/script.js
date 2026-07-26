770843cbca79b144da7ac98c83c2adda17d2a77b
ALPHONS JINCE <alphonsjincea@gmail.com>
2026-07-26 18:24:45 +0530

    feat: solve regEx accenpting unique characters

--- JS_lectures/DOM/script.js
+++ JS_lectures/DOM/script.js
@@ -1,17 +1,21 @@
 let inp = document.querySelector("#text");
 let h1 = document.querySelector("h1");
 
-const lower = "abcdefghijklmnopqrstuvwxyz";
-const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// const lower = "abcdefghijklmnopqrstuvwxyz";
+// const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+// inp.addEventListener("input", function () {
+//   let result = "";
+
+//   for (let ch of inp.value) {
+//     if (lower.includes(ch) || upper.includes(ch) || ch === " ") {
+//       result += ch;
+//     }
+//   }
+
+//   h1.innerText = result;
+// });
 
 inp.addEventListener("input", function () {
-  let result = "";
-
-  for (let ch of inp.value) {
-    if (lower.includes(ch) || upper.includes(ch) || ch === " ") {
-      result += ch;
-    }
-  }
-
-  h1.innerText = result;
+  h1.innerText = inp.value.replace(/[^a-zA-Z ]/g, "");
 });
