06a66c51aff143e140ab027433be6ccc18196e68
Alphons Jince <alphonsjincea@gmail.com>
2026-08-02 23:30:29 +0530

    feat: learnt single threading and call back heel

--- JS_lectures/Adv_JS/script.js
+++ JS_lectures/Adv_JS/script.js
@@ -1,14 +1,30 @@
-function one() {
-    return 1;
+h1 = document.querySelector("h1");
+
+// setTimeout(() => {
+//     h1.style.color = "red";
+// }, 1000);
+
+// setTimeout(() => {
+//     h1.style.color = "red";
+// }, 2000);
+
+// setTimeout(() => {
+//     h1.style.color = "red";
+// }, 3000);
+
+function changeColor(color, delay, nextColor) {
+    setTimeout(() => {
+        h1.style.color = color;
+        if(nextColor) nextColor();
+    }, delay);
 }
 
-function two() {
-    return one() + one();
-}
-
-function three() {
-    let ans = two() + one();
-    console.log(ans);
-}
-
-three();
+changeColor("red", 1000, () => {
+    changeColor("orange", 1000, () => {
+        changeColor("blue", 1000, () => {
+            changeColor("green", 1000, () => {
+                changeColor("grey", 1000);
+            });
+        });
+    });
+});
