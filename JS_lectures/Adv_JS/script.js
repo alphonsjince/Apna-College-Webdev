deee3fca2485d5e8c6740ef98658259658c7e115
Alphons Jince <alphonsjincea@gmail.com>
2026-08-05 23:20:41 +0530

    feat: learnt async and await and applied to color change and rand no

--- JS_lectures/Adv_JS/script.js
+++ JS_lectures/Adv_JS/script.js
@@ -1,70 +1,3 @@
-// function saveToDb(data, success, failure) {
-//     let internetSpeed = Math.floor(Math.random() * 10) + 1;
-//     if (internetSpeed > 4) {
-//         success();
-//     } else {
-//         failure();
-//     }
-// };
-
-// saveToDb(
-//     "hello",
-//     () => {
-//         console.log("success1: data saved");
-//         saveToDb(
-//             "world", () => {
-//                 console.log("success2: data saved");
-//                 saveToDb("alphons",
-//                     () => {
-//                         console.log("sucess 3: saved");
-//                     },
-//                     () => {
-//                         console.log("failure 3 not saved");
-//                     }
-//                 );
-//             },
-//             () => {
-//                 console.log("data 2 not saved");
-//             }
-//         );
-//     },
-//     () => {
-//     console.log("data not saved");
-//     }
-// );
-
-// function saveToDb(data) {
-//   return new Promise((resolve, reject) => {
-//     let internetSpeed = Math.floor(Math.random() * 10) + 1;
-//     if (internetSpeed > 4) {
-//       resolve("success: data was saved");
-//     } else {
-//       reject("failed to save data");
-//     }
-//   });
-// }
-
-// saveToDb("hello world")
-//   .then((result) => {
-//     console.log("data 1 saved");
-//     console.log("result of promise: ", result);
-//     return saveToDb("alphons");
-//   })
-//   .then((result) => {
-//     console.log("data 2 saved");
-//     console.log("result of promise: ", result);
-//     return saveToDb("jince");
-//   })
-//   .then((result) => {
-//     console.log("data 3 saved");
-//     console.log("result of promise: ", result);
-//   })
-//   .catch((error) => {
-//     console.log("promise was rejected");
-//     console.log("result of error: ", error);
-//   });
-
-
 
 
 h1 = document.querySelector("h1");
@@ -78,32 +11,63 @@ function changeColor(color, delay, ) {
   });
 }
 
-changeColor("red", 1000)
-  .then(() => {
-    console.log("red color was completed");
-    return changeColor("orange", 1000);
-  })
-  .then(() => {
-    console.log("orange color was completed");
-    return changeColor("green", 1000);
-  })
-  .then(() => {
-    console.log("green color was completed");
-    return changeColor("blue", 1000);
-  })
-  .then(() => {
-    console.log("blue color was completed");
-  })
+async function color() {
+  await changeColor("red", 1000);
+  await changeColor("orange", 1000);
+  await changeColor("green", 1000);
+  await changeColor("blue", 1000);
+}
+
+color();
+
+// changeColor("red", 1000)
+//   .then(() => {
+//     console.log("red color was completed");
+//     return changeColor("orange", 1000);
+//   })
+//   .then(() => {
+//     console.log("orange color was completed");
+//     return changeColor("green", 1000);
+//   })
+//   .then(() => {
+//     console.log("green color was completed");
+//     return changeColor("blue", 1000);
+//   })
+//   .then(() => {
+//     console.log("blue color was completed");
+//   })
 
 
 
+// async function greet() {
+//   throw "weak connection";
+//   return "hello";
+// }
 
-// changeColor("red", 1000, () => {
-//   changeColor("orange", 1000, () => {
-//     changeColor("green", 1000, () => {
-//       changeColor("yellow", 1000, () => {
-//         changeColor("blue", 1000);
-//       });
-//     });
-//   });
-// });
+// greet()
+//   .then((result) => {
+//     console.log("promise was resolved:", result);
+//   })
+//   .catch((err) => {
+//     console.log("promise was rejected with error: ",err)
+//   })
+
+
+
+function getNum() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let num = Math.floor(Math.random() * 10) + 1;
+      console.log(num);
+      resolve();
+    }, 1000);
+  });
+}
+
+async function demo() {
+  await getNum();
+  await getNum();
+  getNum();
+}
+
+demo();
