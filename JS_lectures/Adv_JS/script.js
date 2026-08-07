3d01cf29b37f5788de192afeb1d6c68f19da8e98
Alphons Jince <alphonsjincea@gmail.com>
2026-08-07 21:52:34 +0530

    feat: tried all async await try catch problems

--- JS_lectures/Adv_JS/script.js
+++ JS_lectures/Adv_JS/script.js
@@ -35,54 +35,54 @@ async function color() {
 
 color();
 
-// changeColor("red", 1000)
-//   .then(() => {
-//     console.log("red color was completed");
-//     return changeColor("orange", 1000);
-//   })
-//   .then(() => {
-//     console.log("orange color was completed");
-//     return changeColor("green", 1000);
-//   })
-//   .then(() => {
-//     console.log("green color was completed");
-//     return changeColor("blue", 1000);
-//   })
-//   .then(() => {
-//     console.log("blue color was completed");
-//   })
+changeColor("red", 1000)
+  .then(() => {
+    console.log("red color was completed");
+    return changeColor("orange", 1000);
+  })
+  .then(() => {
+    console.log("orange color was completed");
+    return changeColor("green", 1000);
+  })
+  .then(() => {
+    console.log("green color was completed");
+    return changeColor("blue", 1000);
+  })
+  .then(() => {
+    console.log("blue color was completed");
+  })
 
 
 
-// async function greet() {
-//   throw "weak connection";
-//   return "hello";
-// }
+async function greet() {
+  throw "weak connection";
+  return "hello";
+}
 
-// greet()
-//   .then((result) => {
-//     console.log("promise was resolved:", result);
-//   })
-//   .catch((err) => {
-//     console.log("promise was rejected with error: ",err)
-//   })
+greet()
+  .then((result) => {
+    console.log("promise was resolved:", result);
+  })
+  .catch((err) => {
+    console.log("promise was rejected with error: ",err)
+  })
 
 
 
-// function getNum() {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       let num = Math.floor(Math.random() * 10) + 1;
-//       console.log(num);
-//       resolve();
-//     }, 1000);
-//   });
-// }
+function getNum() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let num = Math.floor(Math.random() * 10) + 1;
+      console.log(num);
+      resolve();
+    }, 1000);
+  });
+}
 
-// async function demo() {
-//   await getNum();
-//   await getNum();
-//   getNum();
-// }
+async function demo() {
+  await getNum();
+  await getNum();
+  getNum();
+}
 
-// demo();
+demo();
