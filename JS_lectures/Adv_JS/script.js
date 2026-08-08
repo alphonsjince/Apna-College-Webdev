ba0f643cdd946eb6cacfe0d9985deb7cc39207f4
Alphons Jince <alphonsjincea@gmail.com>
2026-08-08 17:34:38 +0530

    feat: learnt json parse and stringify

--- JS_lectures/Adv_JS/script.js
+++ JS_lectures/Adv_JS/script.js
@@ -1,88 +1,12 @@
+let jsonRes =
+  '{"fact":"The average cat food meal is the equivalent to about five mice.","length":63}';
 
+let validRes = JSON.parse(jsonRes);
+console.log(validRes.fact)
 
-h1 = document.querySelector("h1");
+let student = {
+  name: "alphons",
+  marks: 68
+};
 
-function changeColor(color, delay, ) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      let num = Math.floor(Math.random() * 5) + 1;
-      if (num > 3) {
-        reject("promise rejected");
-      }
-      h1.style.color = color;
-      console.log(`color changed to: ${color}`)
-      resolve("color changed");
-    }, delay);
-  });
-}
-
-async function color() {
-  try {
-    await changeColor("red", 1000);
-    await changeColor("orange", 1000);
-    await changeColor("green", 1000);
-    await changeColor("blue", 1000);
-  } catch(err) {
-    console.log("error caught", err);
-  }
-
-  let a = 5;
-  console.log(a);
-  console.log("new number = ", a + 3);
-}
-
-
-
-color();
-
-changeColor("red", 1000)
-  .then(() => {
-    console.log("red color was completed");
-    return changeColor("orange", 1000);
-  })
-  .then(() => {
-    console.log("orange color was completed");
-    return changeColor("green", 1000);
-  })
-  .then(() => {
-    console.log("green color was completed");
-    return changeColor("blue", 1000);
-  })
-  .then(() => {
-    console.log("blue color was completed");
-  })
-
-
-
-async function greet() {
-  throw "weak connection";
-  return "hello";
-}
-
-greet()
-  .then((result) => {
-    console.log("promise was resolved:", result);
-  })
-  .catch((err) => {
-    console.log("promise was rejected with error: ",err)
-  })
-
-
-
-function getNum() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      let num = Math.floor(Math.random() * 10) + 1;
-      console.log(num);
-      resolve();
-    }, 1000);
-  });
-}
-
-async function demo() {
-  await getNum();
-  await getNum();
-  getNum();
-}
-
-demo();
+console.log(JSON.stringify(student));
