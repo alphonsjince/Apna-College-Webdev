6738a36c9c8425ae759dc897463486c9674a7a05
Alphons Jince <alphonsjincea@gmail.com>
2026-08-09 00:39:19 +0530

    feat: learnt api with async and await

--- JS_lectures/Adv_JS/script.js
+++ JS_lectures/Adv_JS/script.js
@@ -1,21 +1,38 @@
 let url = "https://catfact.ninja/fact";
 
-fetch(url)
-  .then((res) => {
-    return res.json();
-  })
-  .then((data) => {
-    console.log(data.fact);
-    return fetch(url);
-  })
-  .then((res) => {
-    return res.json();
-  })
-  .then((data2) => {
-    console.log(data2.fact)
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// fetch(url)
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log(data.fact);
+//     return fetch(url);
+//   })
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((data2) => {
+//     console.log(data2.fact)
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
-console.log("doesnt wait for api it is async");
+
+
+async function getFacts() {
+  try {
+    let res = await fetch(url);
+    let data = await res.json();
+
+    let res2 = await fetch(url);
+    let data2 = await res2.json();
+
+    console.log(data.fact);
+    console.log(data2.fact)
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+getFacts();
