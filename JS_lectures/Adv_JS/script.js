d47c6d3a8f087be45dae4bbfcf4b0b411c9b2b1e
Alphons Jince <alphonsjincea@gmail.com>
2026-08-09 00:22:34 +0530

    feat: multiple json promises on api

--- JS_lectures/Adv_JS/script.js
+++ JS_lectures/Adv_JS/script.js
@@ -2,12 +2,20 @@ let url = "https://catfact.ninja/fact";
 
 fetch(url)
   .then((res) => {
-    console.log(res);
     return res.json();
   })
   .then((data) => {
     console.log(data.fact);
+    return fetch(url);
+  })
+  .then((res) => {
+    return res.json();
+  })
+  .then((data2) => {
+    console.log(data2.fact)
   })
   .catch((err) => {
     console.log(err);
-})
+  });
+
+console.log("doesnt wait for api it is async");
