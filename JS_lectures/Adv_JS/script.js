490a235a980cd620947270a35e0b0b76282e5b12
Alphons Jince <alphonsjincea@gmail.com>
2026-08-10 13:34:34 +0530

    feat: made search clg based on country with api

--- JS_lectures/Adv_JS/script.js
+++ JS_lectures/Adv_JS/script.js
@@ -1,13 +1,32 @@
-const url = "https://icanhazdadjoke.com/";
+let url = "http://universities.hipolabs.com/search?name=";
 
-async function getJokes() {
-  try {
-    const config = { headers: { Accept: "application/json" } };
-    let res = await axios.get(url,config);
-    console.log(res.data);
-  } catch (err) {
-    console.log(err);
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", async () => {
+  let countryName = document.querySelector("input").value;
+  console.log(countryName);
+
+  let clgArr = await getColleges(countryName);
+  show(clgArr);
+});
+
+function show(clgnames) {
+  let list = document.querySelector("#list");
+  list.innerText = "";
+  for (col of clgnames) {
+    console.log(col.name);
+
+    let li = document.createElement("li");
+    li.innerText = col.name;
+    list.appendChild(li);
   }
 }
+async function getColleges(country) {
 
-getJokes();
+  try {
+    let res = await axios.get(url + country);
+    return res.data;
+  } catch (e) {
+    return [];
+  }
+}
