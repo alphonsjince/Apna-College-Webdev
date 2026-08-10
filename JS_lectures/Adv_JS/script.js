ab02f9fcbcec5371dcd78b468f23685dba5d7e1d
Alphons Jince <alphonsjincea@gmail.com>
2026-08-10 12:15:04 +0530

    feat: learnt axios and implemented with html

--- JS_lectures/Adv_JS/script.js
+++ JS_lectures/Adv_JS/script.js
@@ -1,38 +1,23 @@
 let url = "https://catfact.ninja/fact";
 
-// fetch(url)
-//   .then((res) => {
-//     return res.json();
-//   })
-//   .then((data) => {
-//     console.log(data.fact);
-//     return fetch(url);
-//   })
-//   .then((res) => {
-//     return res.json();
-//   })
-//   .then((data2) => {
-//     console.log(data2.fact)
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
+let btn = document.querySelector("button");
 
-
+btn.addEventListener("click", async () => {
+  let factBroweser = await getFacts();
+  let p = document.querySelector('p');
+  p.innerText = factBroweser;
+})
 
 async function getFacts() {
   try {
-    let res = await fetch(url);
-    let data = await res.json();
+    let res = await axios.get(url);
 
-    let res2 = await fetch(url);
-    let data2 = await res2.json();
-
-    console.log(data.fact);
-    console.log(data2.fact)
-  } catch (e) {
-    console.log(e);
+    return res.data.fact;
+  } catch(err) {
+    console.log(err);
+    return "no fact found";
   }
 }
 
 getFacts();
+
