b05c7c623e99d98eba1cf0be6a0877b9ca14cc6e
Alphons Jince <alphonsjincea@gmail.com>
2026-08-12 19:48:19 +0000

    feat: learnt use get nodemon

--- ExpressDir/index.js
+++ ExpressDir/index.js
@@ -8,7 +8,19 @@ app.listen(port, () => {
   console.log(`app is listening on port ${port}`);
 });
 
-app.use((req, res) => {
-  console.log("request recieved");
-  res.send("This is a basic response");
-})
+app.get("/", (req, res) => {
+  res.send("hello im root");
+});
+
+app.get("/search", (req, res) => {
+  res.send("You contacted serarch route");
+});
+
+app.get("/help", (req, res) => {
+  res.send("You contacted help route");
+});
+
+// app.use((req, res) => {
+//   console.log("request recieved");
+//   res.send("This is a basic response");
+// })
