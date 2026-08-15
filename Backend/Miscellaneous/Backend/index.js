c6d85a36ae3f31a6794005100ba261ae38ca088b
Alphons Jince <alphonsjincea@gmail.com>
2026-08-15 18:07:04 +0000

    feat: learnt how to handle post requests

--- Backend/Miscellaneous/Backend/index.js
+++ Backend/Miscellaneous/Backend/index.js
@@ -2,13 +2,16 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/register", (req, res) => {
   let { user, password } = req.query;
   res.send(`Stnadard GET response , welcome ${user}`);
 });
 
 app.post("/register", (req, res) => {
-  res.send("Stnadard POST response");
+  console.log(req.body);
+  res.send(`Stnadard GET response , welcome ${user}`);
 });
 
 app.listen(port, () => {
