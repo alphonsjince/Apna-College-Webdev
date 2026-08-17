0e8894f8c91c73ff1e1c361766478b577de98709
Alphons Jince <alphonsjincea@gmail.com>
2026-08-17 15:34:46 +0000

    feat: learning RESTful APIs and learnt GET implementation

---
+++ rest_class/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+  {
+    username: "apna college",
+    content:"I love coding!"
+  },
+  {
+    username: "alphons jince",
+    content:"Hardwork beats talent!"
+  },
+  {
+    username: "olivia jince",
+    content:"studying hard got an internship"
+  }
+];
+
+app.get("/posts", (req, res) => {
+  res.render("index.ejs", { posts });
+})
+
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+})
