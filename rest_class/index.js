cb9d62ca124ec2042a2ab4f98bf536f494f44c15
Alphons Jince <alphonsjincea@gmail.com>
2026-08-17 19:37:06 +0000

    feat: added view single posts

--- rest_class/index.js
+++ rest_class/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+const { v4: uuidv4 } = require('uuid');
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -12,14 +13,17 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
   {
+    id: uuidv4(),
     username: "apna college",
     content:"I love coding!"
   },
   {
+    id: uuidv4(),
     username: "alphons jince",
     content:"Hardwork beats talent!"
   },
   {
+    id: uuidv4(),
     username: "olivia jince",
     content:"studying hard got an internship"
   }
@@ -35,8 +39,16 @@ app.get("/posts/new", (req, res) => {
 
 app.post("/posts", (req, res) => {
   let { username, content } = req.body;
-  posts.push({ username, content });
+  let id = uuidv4();
+  posts.push({ id, username, content });
   res.redirect('/posts');
+})
+
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  console.log(id);
+  let post = posts.find((p) => id === p.id);
+  res.render("show.ejs", { post });
 })
 
 app.listen(port, () => {
