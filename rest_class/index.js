9df4def1e7ce088fc2a42683280d626f9ee90426
Alphons Jince <alphonsjincea@gmail.com>
2026-08-18 15:16:56 +0000

    feat: made edit on every post

--- rest_class/index.js
+++ rest_class/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const methodOverride = require('method-override')
 const path = require("path");
 const { v4: uuidv4 } = require('uuid');
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -49,7 +51,23 @@ app.get("/posts/:id", (req, res) => {
   console.log(id);
   let post = posts.find((p) => id === p.id);
   res.render("show.ejs", { post });
-})
+});
+
+app.patch("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let newContent = req.body.content;
+  console.log(id);
+  let post = posts.find((p) => id === p.id);
+  post.content = newContent;
+  console.log(post);
+  res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit",(req, res)=> {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  res.render("edit.ejs", { post });
+});
 
 app.listen(port, () => {
   console.log(`Listening to port ${port}`);
