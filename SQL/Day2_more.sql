3da12ff745edb03240480731538122e9a95945cc
Alphons Jince <alphonsjincea@gmail.com>
2026-08-20 18:07:58 +0000

    feat: learnt group byand diff other operators

---
+++ SQL/Day2_more.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE IF NOT EXISTS instagram;
+
+USE instagram;
+
+CREATE TABLE user (
+    id INT,
+    age INT,
+    name VARCHAR(30),
+    email VARCHAR(50),
+    followers INT,
+    following INT,
+    CONSTRAINT check_age CHECK (age >= 13),
+    PRIMARY KEY (id)
+);
+
+INSERT INTO user
+(id, age, name, email, followers, following)
+VALUES
+(1, 14, "adam", "adam@gmail.com", 123, 145),
+(2, 15, "bob", "bob123@gmail.com", 200, 200),
+(3, 16, "casey", "casey@email.com", 300, 306),
+(4, 17, "donald", "donald@gmail.com", 200, 105),
+(5, 14, "eve", "eve@yahoo.in", 400, 145),
+(6, 16, "farah", "farah@gmail.com", 10000, 1000);
+
+select age,count(id)
+from user
+group by age;
