f481c66482757b69fdd94ee4a33960b874d1db70
Alphons Jince <alphonsjincea@gmail.com>
2026-08-21 18:49:44 +0000

    feat: connected node and sql

---
+++ SQLclass/index.js
@@ -0,0 +1,26 @@
+const { faker } = require('@faker-js/faker');
+
+const connection = await mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'Alphons_new',
+  password: 'stalphy@123'
+});
+
+try {
+  connection.query("SHOW TABLES", (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  })
+} catch (err) {
+  console.log(err);
+}
+
+let getRandomUser = () => {
+  return {
+    id: faker.string.uuid(),
+    username: faker.internet.username(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
+}
