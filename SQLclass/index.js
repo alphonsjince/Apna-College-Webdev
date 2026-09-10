9812c4822c71be65332383bab48f6c8a85228473
Alphons Jince <alphonsjincea@gmail.com>
2026-09-10 14:31:44 +0000

    feat: show how many users and show all users routes created

--- SQLclass/index.js
+++ SQLclass/index.js
@@ -1,5 +1,11 @@
 const { faker } = require('@faker-js/faker');
 const mysql = require("mysql2");
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -17,23 +23,37 @@ let getRandomUser = () => {
   ];
 }
 
-let q = "INSERT INTO user (id, username, email, password) VALUES ?"; 
+app.get("/", (req, res) => {
+  let q = `SELECT count(*) FROM user`;
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
+      let count = result[0]["count(*)"];
+      res.render("home.ejs", { count });
+    });
+  } catch (err) {
+    console.log(err);
+    res.send("some error in db");
+  }
+});
 
-let data = [];
+app.get("/user", (req, res) => {
+  let q = `SELECT * FROM user`;
 
-for (i = 1; i <= 100; i++){
-  data.push(getRandomUser());
-}
+  try {
+    connection.query(q, (err, users) => {
+      if (err) throw err;
+      res.render("showusers.ejs", {users});
 
-try {
-  connection.query(q, [data], (err, result) => {
-    if (err) throw err;
-    console.log(result);
-  });
-} catch (err) {
-  console.log(err);
-}
+    })
+  } catch (err) {
+    console.log(err);
+    res.send("some error in db");
+  }
+})
 
-connection.end();
+app.listen("8080", () => {
+  console.log("app is listening on port 8080");
+});
 
 
